4d8052861ce36424784791ec95e80e9ee1c0dc73
rxjas <christopherrojas35@yahoo.com>
2021-04-07 20:17:27 -0600

    Note to self, never make the keys in an onject the same

--- src/App.js
+++ src/App.js
@@ -3,23 +3,25 @@ import Header from './header'
 import Header2 from './header2'
 import { Message, Message2 } from './message'
 
-const headerData = [
-  {text: 'Hello'},
-  {text: 'World'},
-  {text: 'HEADer'}
-]
+const headerData = {text: 'Hello object', text1: 'World Object', text2: 'HEADer Object'};
+
+
 
 function App() {
   return (
     <>
     //time to map through the array
-    {headerData.map((data) => {
+    {/* {headerData.map((data, index) => {
 
       //return the header element per every item
       return(
-        <Header2 text={data.text}></Header2>
+        <Header2 
+        text={data.text}
+        key={index}
+        ></Header2>
       )
-    })}
+    })} */}
+    <Header2 title='Header2 text' text={headerData} ></Header2>
 
     <Header myTitle = "go time">
       <h1>Test time</h1>
--- src/header2.jsx
+++ src/header2.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
 
+const headerData = {text: 'Hello object', text: 'World Object', text: 'HEADer Object'};
+const headerData2= [{text: 'Hello'}, {text: 'World'}, {text: 'HEADer'}];
+
 function Header2 (props){
+  console.log(Object.keys(props.text))
+
     return(
         <>
-        <h3>{props.text}</h3>
+        {/* <div>
+         {headerData2.map((data, index) => {
+            //return the header element per every item
+            return(
+                <p key={index}>{data.text}</p>
+            )
+        })}
+        </div> */}
+
+        <div>
+         {Object.keys(props.text).map((key) => {
+             return(
+                 <li >{props.text[key]}</li>
+             )
+         })}
+        </div>
+
+
+        <h3>{props.title}</h3>
         </>
     )
 };
