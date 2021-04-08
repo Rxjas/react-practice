0da9cc44a72824caa20dc08b008ebf5a54f2f482
rxjas <christopherrojas35@yahoo.com>
2021-04-08 14:41:04 -0600

    Messing around with state

--- src/App.js
+++ src/App.js
@@ -1,36 +1,26 @@
-import React from 'react'
-import Header from './header'
-import Header2 from './header2'
-import { Message, Message2 } from './message'
+import React, { Component } from 'react';
 
-const headerData = {text: 'Hello object', text1: 'World Object', text2: 'HEADer Object'};
+class App extends React.Component {
+  constructor(){
+    super();
 
+    this.state = {
+      count: 0 
+    };
 
+    this.name = "testet tester"
+  }
 
-function App() {
-  return (
-    <>
-    //time to map through the array
-    {/* {headerData.map((data, index) => {
+  // Cant define return, working in an object
+  render() {
+    return(
+      <div>
+        Your count is: {this.state.count}
+        <button onClick={() => this.setState({ count: ++this.state.count })}>Increment count</button>
+      </div>
+    )
+  }
 
-      //return the header element per every item
-      return(
-        <Header2 
-        text={data.text}
-        key={index}
-        ></Header2>
-      )
-    })} */}
-    <Header2 title='Header2 text' ></Header2>
-
-    <Header myTitle = "go time">
-      <h1>Test time</h1>
-    </Header>
-    <Message></Message>
-    <Message2></Message2>
-    <h1>Test Hooray</h1>
-    </>
-  );
-}
+};
 
 export default App;
