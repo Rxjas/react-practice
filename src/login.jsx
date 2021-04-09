0ac03d452fe8c49a4d3e715e5cbc8486af31977e
rxjas <christopherrojas35@yahoo.com>
2021-04-08 22:45:03 -0600

    time to play with more state and forms

---
+++ src/login.jsx
@@ -0,0 +1,31 @@
+// import React, { Component } from 'react';
+
+// class App extends React.Component{
+//   state = {
+//     firstName: 'John',
+//     lastName: 'Doe'
+//   }
+
+//   handleLogin = (event) => {
+//    event.preventDefault();
+//   };
+
+//   render(){
+//   return (
+//     <>
+//     <div>
+//       <h1>Login</h1>
+
+//       <form>
+//         <input type='text' placeholder='First Name' value={this.state.firstName}></input>
+//         <input type='text' placeholder='Last Name' value={this.state.lastName}></input>
+//         <button onClick={this.handleLogin}>Submit</button>
+//       </form>
+      
+//     </div>
+//     </>
+//   )
+// }
+// };
+
+// export default App;
