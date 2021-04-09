423a35e5b446ac574f2920fc92bb8da95652aeea
rxjas <christopherrojas35@yahoo.com>
2021-04-09 17:04:04 -0600

    Added a few things

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App extends React.Component{
   render(){
     return (
       <div>
-        <Header/>
+        <button>Fetch Users</button>
       </div>
     )
 }
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,6 +1,21 @@
 import React, { Component } from 'react';
 
 class Header extends Component {
+    Constructor(){
+        super()
+        this.state = {
+            firstName: '',
+            lastName: '',
+            count: 0 
+        };
+
+        handleInputChange = event => {
+            this.setState({
+                [event.target.name]: event.target.value
+            })
+        }
+    }
+
     render(){
         return(
             <>
